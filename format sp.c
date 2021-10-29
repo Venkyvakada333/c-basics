6474d91f5306f22a380e5fac9e8e77c75c05e2b0
Venkyvakada333 <92910129+Venkyvakada333@users.noreply.github.com>
2021-10-28 23:45:48 -0700

    Add files via upload

---
+++ format sp.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+	int age;
+	float cgpa;
+    char initial;
+	age=20;
+	cgpa=9.0;
+	initial='v';
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+}
