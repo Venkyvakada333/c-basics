fbd2a18c056a4001d5d731c8c5cb9de917d86de4
Venkyvakada333 <92910129+Venkyvakada333@users.noreply.github.com>
2021-10-30 11:44:13 +0530

    Add files via upload

---
+++ formatsp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main( )
+{
+   //variabes
+   int num1,num2,num3,sum;
+
+   //initialisation
+   num1=250;
+   num2=330;
+   num3=590;
+
+   //calc
+   sum=num1+num2+num3;
+
+   //o/p
+   printf("%d",sum);
+}
