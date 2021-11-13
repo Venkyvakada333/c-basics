8a718064754166c7387b406e1b77e9e6afbfe58d
Venkyvakada333 <92910129+Venkyvakada333@users.noreply.github.com>
2021-11-13 12:09:09 +0530

    Add files via upload

---
+++ venky.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int p,t;
+	float r,si;
+	printf("enter any p num ");
+	scanf("%d",&p);
+	printf("enter any t num ");
+	scanf("%d",&t);
+	printf("enter any r num ");
+	scanf("%f",&r);
+	si=(p*t*r)/100;
+	printf("si is %f",si);
+}
